171edf307a6b192758c5a209973fca66f4e3a286
aaayush2604 <aayushfabwani2@gmail.com>
2026-02-17 21:33:39 +0530

    Token class defined

--- internal/query/parser/token.go
+++ internal/query/parser/token.go
@@ -1,7 +1,23 @@
 package query
 
+import "fmt"
+
 type Token struct {
 	TokenType TokenType
 	Lexeme    string
+	Literal   any
 	Position  int
+}
+
+func New(tType TokenType, lex string, lit any, pos int) *Token {
+	return &Token{
+		TokenType: tType,
+		Lexeme:    lex,
+		Literal:   lit,
+		Position:  pos,
+	}
+}
+
+func (t *Token) String() string {
+	return fmt.Sprintf("%v %s %v", t.TokenType, t.Lexeme, t.Literal)
 }
